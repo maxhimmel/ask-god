1eaf9a1740ae73e3e6686ff62a96f41c19485cf9
Max Himmel <himmelmax@gmail.com>
2024-08-13 15:54:46 -0700

    install daisy ui package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
+
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +11,11 @@ export default {
       },
     },
   },
-  plugins: [],
+
+  plugins: [require('daisyui')],
+
+  daisyui: {
+    themes: ["emerald"]
+  },
+
 } satisfies Config;
