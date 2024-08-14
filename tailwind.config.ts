b9504ab78a3d47994d14f0e92f90505d5b0a0e3c
Max Himmel <himmelmax@gmail.com>
2024-08-14 15:01:43 -0700

    fix tailwind warning

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
 
   theme: {
     extend: {
