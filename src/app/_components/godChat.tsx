0f993ea9b0c583520cc04c17836e96f48bc3e13e
Max Himmel <himmelmax@gmail.com>
2024-08-19 11:02:17 -0700

    minor cleanup

--- src/app/_components/godChat.tsx
+++ src/app/_components/godChat.tsx
@@ -1,53 +1,49 @@
 "use client";
 
-import { readStreamableValue } from "ai/rsc";
 import { useState } from "react";
-import { generate } from "~/app/chat/actions";
+import { ChatHistory } from "~/app/_components/chatHistory";
+import { DeityPicker } from "~/app/_components/deityPicker";
 import { api } from "~/trpc/react";
 
 export function GodChat() {
-  const [prompt, setPrompt] = useState<string>("");
   const [generation, setGeneration] = useState<string>("");
 
   const createChat = api.ai.streamChat.useMutation();
 
   return (
     <div>
-      <textarea
-        className="textarea textarea-bordered"
-        value={prompt}
-        onChange={(e) => {
-          setPrompt(e.target.value);
-        }}
-      ></textarea>
+      <form
+        className="flex flex-col gap-2"
+        action={async (form) => {
+          const deity = form.get("deity") as string;
+          const prompt = form.get("question") as string;
 
-      <button
-        className="btn btn-primary btn-lg"
-        onClick={async () => {
           const chatIterator = await createChat.mutateAsync({
-            hp: "Buddha",
+            hp: deity,
             question: prompt,
-          }); //generate(prompt);
+          });
 
           for await (const { output } of chatIterator) {
-            console.log({ output });
             setGeneration(
               (currentGeneration) => `${currentGeneration}${output}`,
             );
           }
-          // for await (const delta of readStreamableValue(output)) {
-          //   setGeneration(
-          //     (currentGeneration) => `${currentGeneration}${delta}`,
-          //   );
-          // }
         }}
       >
-        Chat
-      </button>
+        <DeityPicker />
 
-      <div>
-        <pre className="font-sans">{generation}</pre>
-      </div>
+        <textarea
+          name="question"
+          className="textarea textarea-bordered"
+          placeholder="What's on your mind?"
+        ></textarea>
+
+        <button type="submit" className="btn btn-primary btn-lg">
+          Chat
+        </button>
+      </form>
+
+      <ChatHistory>{generation}</ChatHistory>
     </div>
   );
 }
