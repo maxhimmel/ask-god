b0372dd8f4a47f38099f39809d7a9d44c0128235
Max Himmel <himmelmax@gmail.com>
2024-08-19 11:02:00 -0700

    minor cleanup

--- src/app/_components/askGod.tsx
+++ src/app/_components/askGod.tsx
@@ -2,24 +2,21 @@
 
 import { api } from "~/trpc/react";
 import { useState } from "react";
-
-const deities = ["Jesus", "Buddha", "Krishna", "Allah"];
+import { DeityPicker } from "~/app/_components/deityPicker";
+import { ChatHistory } from "~/app/_components/chatHistory";
 
 export function AskGod() {
   const [answer, setAnswer] = useState<string | undefined>(undefined);
 
-  const createAnswer = api.ai.ask.useMutation();
+  const createAnswer = api.ai.getAnswer.useMutation();
 
   return (
     <div>
       <form
         className="flex flex-col gap-2"
-        onSubmit={async (form) => {
-          form.preventDefault();
-
-          const formData = new FormData(form.target as HTMLFormElement);
-          const deity = formData.get("deity") as string;
-          const question = formData.get("question") as string;
+        action={async (form) => {
+          const deity = form.get("deity") as string;
+          const question = form.get("question") as string;
 
           const response = await createAnswer.mutateAsync({
             hp: deity,
@@ -29,22 +26,7 @@ export function AskGod() {
           setAnswer(response.answer);
         }}
       >
-        <select
-          name="deity"
-          defaultValue={""}
-          className="select select-bordered"
-          required
-        >
-          <option disabled value={""}>
-            Pick a deity:
-          </option>
-
-          {deities.map((deity) => (
-            <option key={deity} value={deity}>
-              {deity}
-            </option>
-          ))}
-        </select>
+        <DeityPicker />
 
         <textarea
           name="question"
@@ -58,7 +40,7 @@ export function AskGod() {
         </button>
       </form>
 
-      <div className="">{answer}</div>
+      <ChatHistory>{answer}</ChatHistory>
     </div>
   );
 }
--- src/server/api/routers/ai.ts
+++ src/server/api/routers/ai.ts
@@ -1,26 +1,12 @@
 import { google } from "@ai-sdk/google";
 import { generateText, streamText } from "ai";
-import { createStreamableValue } from "ai/rsc";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const aiRouter = createTRPCRouter({
-    ask: publicProcedure
-        .input(z.object({ hp: z.string(), question: z.string() }))
-        .mutation(async ({ ctx, input }) => {
-            const { text } = await generateText({
-                model: google("models/gemini-1.5-flash-latest"),
-                prompt: input.question,
-                system: `You are ${input.hp}.`
-            });
-
-            return {
-                answer: text,
-            };
-        }),
     getAnswer: publicProcedure
         .input(z.object({ hp: z.string(), question: z.string() }))
-        .query(async ({ input }) => {
+        .mutation(async ({ ctx, input }) => {
             const { text } = await generateText({
                 model: google("models/gemini-1.5-flash-latest"),
                 prompt: input.question,
@@ -44,23 +30,5 @@ export const aiRouter = createTRPCRouter({
             for await (const delta of textStream) {
                 yield { output: delta };
             }
-            // const stream = createStreamableValue("");
-
-            // (async () => {
-            //     const { textStream } = await streamText({
-            //         model: google("models/gemini-1.5-flash-latest"),
-            //         system: `You are ${input.hp}.`,
-            //         prompt: input.question,
-            //     });
-
-            //     for await (const delta of textStream) {
-            //         console.log({ delta });
-            //         stream.update(delta);
-            //     }
-
-            //     stream.done();
-            // })();
-
-            // yield { output: stream.value };
         }),
 });
