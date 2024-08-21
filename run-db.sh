9ae099f109b69ff521be706c51cc47083ad2bf9d
Max Himmel <himmelmax@gmail.com>
2024-08-21 13:33:08 -0700

    add command and script for more quickly starting full dev environment
    + spin up the db container if it's not running as well as ensuring it has latest prisma updates

---
+++ run-db.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Use this script to replace the database with a fresh one
+
+DB_CONTAINER_NAME="ask-god-mysql"
+
+if ! [ -x "$(command -v docker)" ]; then
+    echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
+    exit 1
+fi
+
+if ! docker info > /dev/null 2>&1; then
+    echo "Starting Docker Desktop..."
+    "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+fi
+
+while ! docker system info > /dev/null 2>&1; do
+    echo "Waiting for Docker to start..."
+    sleep 1
+done
+
+echo "Starting database container:"
+./start-database.sh
+
+while ! docker exec -i "$DB_CONTAINER_NAME" mysqladmin ping -h"localhost" --silent; do
+    echo "Waiting for database to start..."
+    sleep 1
+done
+
+sleep 3
+
+echo "Pushing database schema and seeding data..."
+npm run db:push
+npm run db:seed
