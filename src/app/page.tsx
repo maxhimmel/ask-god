4957fd0c439340cccb98dc88ee507d12059bb715
Max Himmel <himmelmax@gmail.com>
2024-08-22 14:33:42 -0700

    update home page to have a small prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
-import { redirect } from "next/navigation";
-import { getServerAuthSession } from "~/server/auth";
+import Link from "next/link";
+import { UserSplit } from "~/app/_components/userDisplays";
+import { HydrateClient } from "~/trpc/server";
 
-export default async function Home() {
-  const session = await getServerAuthSession();
-  if (!session) {
-    return redirect("/api/auth/signin");
-  }
-
-  redirect("/chat");
+export default function Home() {
+  return (
+    <HydrateClient>
+      <main className="hero">
+        <div className="hero-content flex h-screen w-screen flex-col gap-6 bg-base-200">
+          <div className="flex flex-col gap-4 px-52 text-center">
+            <h1 className="text-6xl font-semibold">
+              {/* Have a chat with your own conception of a Higher Power */}
+              If you could ask God one question, what would it be?
+            </h1>
+            <p className="text-2xl font-extralight">
+              Get started using AI to chat with your own conception of a Higher
+              Power.
+            </p>
+          </div>
+          <UserSplit>
+            <Link href="/chat" className="btn btn-primary mx-auto">
+              Start Chatting
+            </Link>
+            <Link href="/api/auth/signin" className="btn btn-primary mx-auto">
+              Get Started
+            </Link>
+          </UserSplit>
+        </div>
+      </main>
+    </HydrateClient>
+  );
 }
